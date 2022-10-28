a24c3f84d09b5ed2eb0cd6c83d037008bd61602e
FlorBl <florjanblakaj@hotmail.com>
2022-10-28 17:15:43 -0400

    ..

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -161,6 +161,7 @@ EMAIL_USE_TLS = False
 
 
 # Method 2: Production Way
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'developer.testmail2023@gmail.com'
