d0a4d0f7624410004bb77f5fadc941bb3f861403
FlorBl <florjanblakaj@hotmail.com>
2022-10-30 10:07:01 -0400

    //

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -54,7 +54,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'developer.testmail2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'joovhcxgwsstmqia'
+EMAIL_HOST_PASSWORD = 'idktmasuuoygtwsp'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
